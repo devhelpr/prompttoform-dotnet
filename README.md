3437c80d0cc1dd538c1c434af80f06aeed3de1d4
Maikel van de Lisdonk <maikel@devhelpr.com>
2025-05-18 12:40:32 +0200

    Implement JWT authentication and authorization for time API; update README with usage instructions

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ dotnet run
 in the browser navigate to `http://localhost:5256/time` to see the current time.
 
 for the scalar openapi UI navigate to `http://localhost:5256/scalar/` to see the API documentation.
+
+dotnet user-jwts create --scope "time_api" --role "admin"
+
+use Authorization header with the token generated above to access the API : Bearer <token>
--- Time.Api/Program.cs
+++ Time.Api/Program.cs
@@ -2,7 +2,15 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthorization();
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthorizationBuilder()
+  .AddPolicy("time", policy =>
+        policy
+            .RequireRole("admin")
+            .RequireClaim("scope", "time_api"));
 
 var app = builder.Build();
 
@@ -12,13 +20,13 @@ app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/time", () => 
+app.MapGet("/time", () =>
 {
-    return new 
+    return new
     {
         CurrentTime = DateTime.UtcNow,
         TimeZone = "UTC"
     };
-});
+}).RequireAuthorization("time");
 
 app.Run();
