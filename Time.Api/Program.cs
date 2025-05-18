7395600481698a0a7cd64ca8d2f997ec2270f11e
Maikel van de Lisdonk <maikel@devhelpr.com>
2025-05-18 13:17:03 +0200

    Configure JWT authentication with authority and audience settings in appsettings.json

--- Time.Api/Program.cs
+++ Time.Api/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication().AddJwtBearer();
+//builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["JwtBearer:Authority"];
+        options.Audience = builder.Configuration["JwtBearer:Audience"];
+    });
+
 builder.Services.AddAuthorization();
 builder.Services.AddOpenApi();
 
